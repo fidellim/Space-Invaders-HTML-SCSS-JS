c696aa344cfc9e8e1f319232e585d18601b3306a
fidellim <lim.fidel@gmail.com>
2022-05-05 12:50:00 +0400

    add grid for invaders

---
+++ grid.js
@@ -0,0 +1,52 @@
+import Invader from "./invader.js";
+
+export default class Grid {
+	constructor(c, canvas) {
+		this.canvas = canvas;
+		this.position = {
+			x: 0,
+			y: 0,
+		};
+
+		this.velocity = {
+			x: 3,
+			y: 0,
+		};
+
+		this.invaders = [];
+
+		const columns = Math.floor(Math.random() * 10 + 5);
+		const rows = Math.floor(Math.random() * 5 + 2);
+
+		this.width = columns * 30;
+
+		for (let x = 0; x < columns; x++) {
+			for (let y = 0; y < rows; y++) {
+				this.invaders.push(
+					new Invader(c, {
+						position: {
+							x: x * 30,
+							y: y * 30,
+						},
+					})
+				);
+			}
+		}
+	}
+
+	update() {
+		this.position.x += this.velocity.x;
+		this.position.y += this.velocity.y;
+
+		this.velocity.y = 0;
+
+		if (
+			this.position.x + this.width >= this.canvas.width ||
+			this.position.x <= 0
+		) {
+			this.velocity.x = -this.velocity.x;
+			// this.velocity.x = -this.velocity.x * 1.15;
+			this.velocity.y = 30;
+		}
+	}
+}
