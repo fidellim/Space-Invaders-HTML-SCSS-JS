507de0a1056d941c25d7123d3bb1148a0b85218f
fidellim <lim.fidel@gmail.com>
2022-05-05 15:55:28 +0400

    add depencies for production and development || update scripts

---
+++ webpack.common.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	entry: {
+		bundle: path.resolve(__dirname, "src/index.js"),
+	},
+	resolve: {
+		extensions: [".js"],
+		modules: ["src", "node_modules"], // Assuming that your files are inside the src dir
+	},
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "[name].[contenthash].js",
+		clean: true,
+		assetModuleFilename: "[name][ext]",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"],
+					},
+				},
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: "asset/resource",
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "Space Invaders",
+			filename: "index.html",
+			template: "src/index.html",
+		}),
+	],
+};
